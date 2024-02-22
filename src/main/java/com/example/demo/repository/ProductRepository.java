115f77dd8d6dcc1ede195f20811d0873c4004d61
enio <enio1303@gmail.com>
2024-02-21 21:27:54 -0300

    Adiciona inicio de produtos

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,4 +1,23 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+@Repository
 public class ProductRepository {
+
+    private ArrayList<Product> product_list = new ArrayList<Product>();
+    public Product createProduct(Product product){
+        product_list.add(product);
+        return product;
+    }
+
+    public Product getProduct(String id){
+        Product product = product_list.stream().filter(p -> p.getProdutc_id().equals((id))).findFirst().orElse(null);
+        return product;
+    }
+
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,4 +1,21 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product save(Product product){
+        return productRepository.createProduct(product);
+    }
+
+    public Product find(String id){
+        return productRepository.getProduct(id);
+    }
 }
